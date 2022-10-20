c6aa18f95bcfe3e1219c04e387ed1017800db625
Trapland <89914789+Trapland@users.noreply.github.com>
2022-10-20 16:21:35 +0200

    BuildStart

--- The choice/ExtraInfo.xaml.cs
+++ The choice/ExtraInfo.xaml.cs
@@ -10,23 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace The_choice
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ExtraWindow : Window
     {
-        public MainWindow()
+        public ExtraWindow()
         {
             InitializeComponent();
-            this.Height = SystemParameters.PrimaryScreenHeight;
-            this.Width = SystemParameters.PrimaryScreenWidth;
-            this.WindowState = WindowState.Maximized;
-
         }
     }
 }
--- The choice/Window1.xaml.cs
+++ The choice/Window1.xaml.cs
@@ -10,23 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace The_choice
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-            this.Height = SystemParameters.PrimaryScreenHeight;
-            this.Width = SystemParameters.PrimaryScreenWidth;
-            this.WindowState = WindowState.Maximized;
-
         }
     }
 }
